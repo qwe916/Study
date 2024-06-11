bf7094d65d4137385d29a20d1aa4f06dfb37d690
hongyoung <qwe916@naver.com>
2024-06-11 16:28:15 +0900

    Data Structure - 선형 자료구조

---
+++ design_pattern_study/src/test/java/com/design/data_structure/deque/DequeTest.java
@@ -0,0 +1,22 @@
+package com.design.data_structure.deque;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeTest {
+    @Test
+    void dequeTest() {
+        Deque<Object> objects = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            objects.offerFirst(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Assertions.assertThat(objects.pollFirst()).isEqualTo(9 - i);
+        }
+    }
+}
---
+++ design_pattern_study/src/test/java/com/design/data_structure/queue/QueueTest.java
@@ -0,0 +1,22 @@
+package com.design.data_structure.queue;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+    @Test
+    void queueTest() {
+        Queue<Object> queue = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            queue.offer(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Assertions.assertThat(queue.poll()).isEqualTo(i);
+        }
+    }
+}
